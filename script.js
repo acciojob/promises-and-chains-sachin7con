3b6b0452416dcde036a4c82ad1ec3ef4712b275d
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-04-04 17:43:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+//SGN your JS code here. If required.
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+
+
+let name = document.getElementById("name").value;
+let age = parseInt(document.getElementById("age").value);
+
+if(!name || !age){
+	alert("Please fill in both the fields");
+	return;
+}
+
+
+const promis = new Promise(function(resolve, reject){
+	setTimeout(() => {
+		if(age>=18){
+			resolve();
+		}else{
+			reject();
+		}
+	}, 4000);
+});
+
+promis.then(() =>{
+	alert(`Welcome, ${name}. You can vote.`);
+}).catch(() => {
+	alert(`Oh sorry ${name}. You aren't old enough.`);
+})
+		
+});
